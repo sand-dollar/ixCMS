f6d3ec34fa97c11b610a5ccd5a949979908f9ec7
Lukáš Bednařík <lukas.bednarik@ixperta.com>
2016-10-11 13:56:07 +0200

    First expressjs + EJS demo

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "google",
+    "rules": {
+        "max-len": [1, 120, 2],  // Max line length
+        "indent": [1, 4],        // Indent with 4 spaces
+        "no-negated-condition": "off",
+        "no-else-return": "off"
+    }
+};
