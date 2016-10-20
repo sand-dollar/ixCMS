f46dda3da229afd2f7ba3b944d9aac9dace56063
Lukáš Bednařík <l.bednarik@gmail.com>
2016-10-20 20:15:00 +0200

    Disabled jsdoc validation (eslint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         "max-len": [1, 120, 2],  // Max line length
         "indent": [1, 2],        // Indent with 2 spaces
         "no-negated-condition": "off",
-        "no-else-return": "off"
+        "no-else-return": "off",
+        "valid-jsdoc": "off"
     }
 };
